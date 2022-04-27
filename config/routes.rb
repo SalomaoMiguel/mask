cb7b66641d4b01d8e79333ba2609e51d60d46c9b
salomaoMiguel <salomaombono@outlook.com>
2022-04-27 19:01:36 -0300

    create user, post and profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
     root to: "home#index"
     resources :admins
     resources :users, only: :index
+    resources :posts, only: [:show, :index]
   end
 
   
   root to: "user/timeline#index"
-  namespace :user do 
+
+  namespace :user do
+     get 'profile', to: "profile#show"
+     resources :posts, only: :create
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ spec/factories/posts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :post do
+    body { "Texto de post" }
+  end
+end
